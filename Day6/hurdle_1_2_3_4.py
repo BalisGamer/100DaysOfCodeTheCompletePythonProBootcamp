15d74d7cb2db6d784b5a7678744dccf214efc672
BalisGamer <balis.gamer@gmail.com>
2024-07-26 09:08:57 +0000

    day_6_hurdle_all

---
+++ Day6/hurdle_1_2_3_4.py
@@ -0,0 +1,20 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    if right_is_clear() == True:
+        if wall_on_right() == False:
+            turn_right()
+            if front_is_clear() == True:
+                move()
+        else:
+            turn_right()
+    else:
+        if front_is_clear() == True:
+            move()
+        elif wall_in_front() == True:
+            turn_left()
+        else:
+            turn_right()
--- Day7/finalProject_step1.py
+++ Day7/finalProject_step1.py
@@ -1,9 +1,12 @@
 #Step 1 
-
+import random
 word_list = ["aardvark", "baboon", "camel"]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-
+chosen_word = random.choice(word_list)
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-
+called_guess = input("guess a letter?").lower()
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+
+print(chosen_word)
+print(called_guess)
